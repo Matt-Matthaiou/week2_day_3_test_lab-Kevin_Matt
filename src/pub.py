10627c350537d93f82c431237b634da10b50c066
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-02 13:43:41 +0000

    completed MVP

--- src/pub.py
+++ src/pub.py
@@ -1,17 +1,27 @@
-
+from src.customer import Customer
 
 class Pub:
     
-    def __init__(self, name, till, drinks):
+    def __init__(self, name, till,):
         self.name = name
         self.till = till
         self.drinks = []
 
-
-    def find_drink_by_name(self, drink_name):
-
-        if drink_name in self.drinks.keys():
-            pass
-
     def increase_till(self, amount):
         self.till += amount
+
+    def add_drink_to_stock(self, drink):
+        self.drinks.append(drink)
+
+    
+    def customer_order(self, drink_name, customer):
+        # print(drink_name)
+        # print(customer)
+        drink_price = 0
+        for item in self.drinks:
+            if item.name == drink_name:
+                drink_price = item.price
+        
+        # print(drink_price)
+        self.increase_till(drink_price)
+        customer.reduce_wallet(drink_price)
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,11 +1,16 @@
 import unittest
 from src.pub import Pub
 from src.drinks import Drinks
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     def setUp(self):
-        self.drinks = Drinks("whisky" , 5)
-        self.pub = Pub("The Prancing Pony", 100.000, self.drinks)
+        self.drink = Drinks("whisky", 5)
+        self.drink2 = Drinks("vodka", 4)
+        self.pub = Pub("The Prancing Pony", 100.000)
+        self.customer = Customer("Matt", 30)
+        self.pub.add_drink_to_stock(self.drink)
+        self.pub.add_drink_to_stock(self.drink2)
         
 
     def test_pub_has_name(self):
@@ -15,18 +20,21 @@ class TestPub(unittest.TestCase):
         self.assertEqual(100.00, self.pub.till)
 
     def test_drink_name(self):
-        self.assertEqual("whisky", self.drinks.name)
+        self.assertEqual("whisky", self.drink.name)
     
     def test_drink_price(self):
-        self.assertEqual (5, self.drinks.price)
+        self.assertEqual (5, self.drink.price)
 
-    # def test_find_drink_by_name(self):
-    #    drink_from_drinks = find_drink_by_name("beer")
-    #    self.assertEqual("vodka", drink_from_drinks)
 
     def test_increase_till(self):
         self.pub.increase_till(2.50)
         expected = 102.50
         actual = self.pub.till
         self.assertEqual(expected, actual)
+
+    def test_customer_order(self):
+        self.pub.customer_order("vodka", self.customer)
+        self.assertEqual (25, self.customer.wallet)
+        self.assertEqual (105, self.pub.till)
+
 
